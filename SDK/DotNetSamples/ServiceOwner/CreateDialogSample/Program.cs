a254ea8e0bb315f9c08f2abcf231ae7f96e509bb
Amund Myrbostad <Lullinj98@gmail.com>
2026-09-07 10:41:08 +0200

    Created simple CreateDialog sample

---
+++ SDK/DotNetSamples/ServiceOwner/CreateDialogSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using Altinn.ApiClients.Dialogporten;
+using Altinn.ApiClients.Dialogporten.ServiceOwner;
+using CreateDialogSample;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Configuration.AddUserSecrets<Program>(optional: true);
+
+var dialogportenSettings = builder.Configuration
+        .GetSection("DialogportenSettings")
+        .Get<DialogportenSettings>()
+ ?? throw new InvalidOperationException(
+        "Configuration section 'DialogportenSettings' is missing or invalid. " +
+        "Ensure appsettings.json is present in the output directory and contains a valid DialogportenSettings section.");
+
+
+builder.Services.AddDialogportenClient(dialogportenSettings);
+
+using var app = builder.Build();
+
+var serviceOwnerApi = app.Services.GetRequiredService<IServiceOwnerApi>().V1;
+
+var ct = CancellationToken.None;
+// var createDialog = CreateDialogDtoSamples.MinimalDialogDto();
+var createDialog = CreateDialogDtoSamples.ComplexDialogDto();
+
+var createDialogResponse = await serviceOwnerApi.CreateDialog(createDialog, ct);
+if (createDialogResponse.IsSuccessStatusCode)
+{
+    Console.WriteLine("Dialog successfully created.");
+    Console.WriteLine(createDialogResponse.Content);
+}
+else
+{
+    Console.WriteLine("Something went wrong.");
+    Console.WriteLine(createDialogResponse.ReasonPhrase);
+    Console.WriteLine(createDialogResponse.Error.Content);
+}
